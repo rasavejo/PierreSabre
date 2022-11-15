bfb1c9785a9159723c082ef1d9c10b6d5a1a2fe6
rumin raphael <rmr4685a@ens.fsi>
2022-11-15 16:38:33 +0100

    ajout memoire

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,9 +1,12 @@
 package personnages;
 
 public class Humain {
+	static int TAILLE_MEMOIRE = 30;
 	private String nom;
 	private String boissonFavorite;
 	private int capital;
+	protected int nbConnaissance = 0;
+	protected Humain[] memoire = new Humain[TAILLE_MEMOIRE] ;
 	
 	public Humain(String nom, String boissonFavorite, int capital) {
 		this.nom = nom;
@@ -46,5 +49,44 @@ public class Humain {
 			parler("J'ai " + capital + " en poche, je vais pouvoir m'offrir un " + bien + " à " + prix + " sous.");
 			perdreArgent(prix);
 		}
+	}
+	
+	private void memoriser(Humain humain) {
+		Boolean connu = false;
+		for (int i = 0;i<nbConnaissance && !connu ;i++) {
+			if (memoire[i] == humain) connu = true;
+		}
+		if (!connu) {
+			if (nbConnaissance < TAILLE_MEMOIRE) {
+				memoire[nbConnaissance] = humain;
+				nbConnaissance ++;
+			} else {
+				for (int i = 0;i<TAILLE_MEMOIRE-1;i++) {
+					memoire[i] = memoire[i+1];
+				}
+				memoire[TAILLE_MEMOIRE-1] = humain;
+			}
+		}
+	}
+	
+	private void repondre(Humain humain) {
+		direBonjour();
+		memoriser(humain);
+	}
+	
+	public void faireConnaissanceAvec(Humain humain) {
+		direBonjour();
+		humain.repondre(this);
+		memoriser(humain);
+	}
+	
+	public void listerConnaissance() {
+		String connaissances = "Je connais beaucoup de monde dont : ";
+		connaissances += memoire[0].getNom();
+		for (int i = 1;i<nbConnaissance;i++) {
+			connaissances += ",";
+			connaissances += memoire[i].getNom();
+		}
+		parler(connaissances);
 	}
 }
